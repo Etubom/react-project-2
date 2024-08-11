832ddfbe02fccf759018216f69f088ece896b554
etubom <way2young4u@hotmail.com>
2024-08-12 00:26:54 +0100

    added product form

--- src/ProductForm.tsx
+++ src/ProductForm.tsx
@@ -1,7 +1,63 @@
+enum Colour {
+  Red,
+  Blue,
+  Green,
+  Yellow,
+  White,
+}
+export interface InterfaceProps {
+  id: number
+  title: string
+  description: string
+  imageUrl: string
+  inStock: boolean
+  colour: Colour
+  price: string
+  inSale: boolean
+}
 const ProductForm = () => {
+  const handleSubmit = () => {
+    console.log('submit clicked')
+  }
   return (
     <>
-      <p>Product Form here</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Product id:</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Title:</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Description:</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Image Link:</label>
+          <span>Link goes here</span>
+        </div>
+        <span>In Sale:</span>
+        <label>Yes</label>
+        <input type="radio" name="inSale" value="yes" />
+        <label>No</label>
+        <input type="radio" name="inSale" value="no" />
+        <br />
+        <span>In Stock:</span>
+        <label>Yes</label>
+        <input type="radio" name="inStock" value="yes" />
+        <label>No</label>
+        <input type="radio" name="inStock" value="no" />
+        <div>
+          <label>Price:</label>
+          <span>{`£ price`}</span>
+        </div>
+        <div>
+          <label>Colour:</label>
+          <span>enum colour here</span>
+        </div>
+      </form>
     </>
   )
 }
